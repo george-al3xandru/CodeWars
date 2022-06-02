8a85a07fc809d1ea125fc758a5f023e83d003e6e
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-02 20:57:17 +0300

    Update Name Shuffler.py

--- 8-kyu/Name Shuffler.py
+++ 8-kyu/Name Shuffler.py
@@ -1,4 +1,5 @@
 # Write a function that returns a string in which firstname is swapped with last name.
+# name_shuffler("john McClane") --> "McClane john"
 
 def name_shuffler(str):
     reverse_str = ""
