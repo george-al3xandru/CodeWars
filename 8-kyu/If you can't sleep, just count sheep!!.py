9b79dc29f3630dcdb48b96894d13819c1c4c7572
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-03 18:58:57 +0300

    Create If you can't sleep, just count sheep!!.py

---
+++ 8-kyu/If you can't sleep, just count sheep!!.py
@@ -0,0 +1,14 @@
+# Task:
+# Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...".
+# Input will always be valid, i.e. no negative integers.
+
+# Solution 1
+def count_sheep(n):
+    s = ""
+    for i in range(1, n+1):
+        s += f"{i} sheep..."
+    return s
+  
+# Solution 2
+def count_sheep(n):
+    return ''.join(f"{i} sheep..." for i in range(1,n+1))
