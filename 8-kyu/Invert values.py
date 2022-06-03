78ae63f0907582054ccfe14dc08d311ebf08a9aa
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-03 18:06:55 +0300

    Create Invert values.py

---
+++ 8-kyu/Invert values.py
@@ -0,0 +1,19 @@
+# Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+# invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+# invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+# invert([]) == []
+# You can assume that all values are integers. Do not mutate the input array/list.
+
+# Solution 1
+def invert(lst):
+    x = 0
+    inv_lst = []
+    while x < len(lst):
+        inv_lst.append(lst[x] * -1)
+        x += 1
+    return inv_lst
+  
+# Solution 2
+  def invert(lst):
+    return [-x for x in lst]
