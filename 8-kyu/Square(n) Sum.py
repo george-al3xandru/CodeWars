63e54d0ee8d170c605a674d4733a136c06217691
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-03 09:33:38 +0300

    Create Square(n) Sum.py

---
+++ 8-kyu/Square(n) Sum.py
@@ -0,0 +1,15 @@
+# Task
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+# For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+# Solutin 1:
+def square_sum(numbers):
+    sum = 0
+    for i in numbers:
+        sum += i ** 2
+    return sum
+  
+# Solutin 2:
+def square_sum(numbers):
+    return sum(x ** 2 for x in numbers)
