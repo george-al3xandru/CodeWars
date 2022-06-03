7e6bba434a16f83acc48b62a2ff5c4c681a1625e
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-03 12:05:00 +0300

    Create Sum of positive.py

---
+++ 8-kyu/Sum of positive.py
@@ -0,0 +1,17 @@
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+
+# Solution 1
+def positive_sum(arr):
+    sum = 0
+    for i in arr:
+        if i > 0:
+            sum += i
+    return sum
+
+# Solution 2
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
