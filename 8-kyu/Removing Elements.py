96f54965bbcf48bcdef899802357a8dbd7af2515
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-03 14:01:47 +0300

    Create Removing Elements.py

---
+++ 8-kyu/Removing Elements.py
@@ -0,0 +1,20 @@
+# Task
+# Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+# Example:
+# ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+# None of the arrays will be empty, so you don't have to worry about that!
+
+# Solution 1
+def remove_every_other(my_list):
+    i = 1
+    while i < len(my_list):
+        my_list.pop(i)
+        i += 1
+    return my_list
+
+# Solution 2
+def remove_every_other(my_list):
+    return my_list[::2]
+    
