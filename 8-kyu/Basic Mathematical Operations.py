d9c596af06b487669539dad4968ffc561abac035
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-05 17:47:14 +0300

    Create Basic Mathematical Operations.py

---
+++ 8-kyu/Basic Mathematical Operations.py
@@ -0,0 +1,25 @@
+# Your task is to create a function that does four basic mathematical operations.
+
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+
+# Examples(Operator, value1, value2) --> output
+# ('+', 4, 7) --> 11
+# ('-', 15, 18) --> -3
+# ('*', 5, 5) --> 25
+# ('/', 49, 7) --> 7
+
+# Solution 1
+def basic_op(operator, value1, value2):
+    if operator == "+":
+        return value1 + value2
+    elif operator == "-":
+        return value1 - value2
+    elif operator == "*":
+        return value1 * value2
+    else:
+        return value1 / value2
+      
+# Solution 2
+def basic_op(operator, value1, value2):
+    return eval(str(value1) + operator + str(value2))
