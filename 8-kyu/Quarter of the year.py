6b19258f46e02b2496d08c6231fe16e3d7488ac2
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-10 19:26:19 +0300

    Create Quarter of the year.py

---
+++ 8-kyu/Quarter of the year.py
@@ -0,0 +1,20 @@
+# DESCRIPTION:
+# Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+# For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+# Solution 1
+def quarter_of(month):
+    if month <= 3:
+        return 1
+    elif month <= 6:
+        return 2
+    elif month <= 9:
+        return 3
+    else:
+        return 4
+      
+# Solution 2
+from math import ceil
+def quarter_of(month):
+    return ceil(month / 3)
