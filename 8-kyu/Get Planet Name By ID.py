aba578483f51c9f732af85806a567e2ae92db04b
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-11 08:08:35 +0300

    Create Get Planet Name By ID.py

---
+++ 8-kyu/Get Planet Name By ID.py
@@ -0,0 +1,17 @@
+# DESCRIPTION:
+# The function is not returning the correct values. Can you figure out why?
+
+# Example (Input --> Output ):
+# 3 --> "Earth"
+
+def get_planet_name(id):
+    return {   
+    1: "Mercury",
+    2: "Venus",
+    3: "Earth",
+    4: "Mars",
+    5: "Jupiter",
+    6: "Saturn",
+    7: "Uranus",  
+    8: "Neptune",
+    }[id]
