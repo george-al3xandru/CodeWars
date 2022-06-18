916edff3200d87cacd843309de8dc9a9d64fa14b
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-18 11:00:30 +0300

    Create sPoNgEbOb MeMe.py

---
+++ 7-kyu/sPoNgEbOb MeMe.py
@@ -0,0 +1,26 @@
+# DESCRIPTION:
+# Remember the spongebob meme that is meant to make fun of people by repeating what they say in a mocking way?
+# You need to create a function that converts the input into this format, with the output being the same string expect there is a
+# pattern of uppercase and lowercase letters.
+
+# Example:
+
+# input:  "stop Making spongebob Memes!"
+# output: "StOp mAkInG SpOnGeBoB MeMeS!"
+
+# Solution 1:
+def sponge_meme(s):
+    output = ""
+    for i in range(len(s)):
+        if i%2==0:
+            output += s[i].upper()
+        else:
+            output += s[i].lower()
+    return output
+  
+# Solution 2:
+def sponge_meme(s):
+    a = list(s)
+    a[::2], a[1::2] = s[::2].upper(), s[1::2].lower()
+    return ''.join(a)
+  
