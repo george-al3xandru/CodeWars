ebd2134028fd0e8d568901562166b8c08586d515
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-21 15:47:50 +0300

    Update Closest elevator.py

--- 8-kyu/Closest elevator.py
+++ 8-kyu/Closest elevator.py
@@ -1,24 +1,36 @@
-# Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered 0 to 2), write a function elevator accepting 3 arguments (in order):
+# Closest elevator
 
-# left - The current floor of the left elevator
-# right - The current floor of the right elevator
-# call - The floor that called an elevator
-# It should return the name of the elevator closest to the called floor ("left"/"right").
+Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered 0 to 2), write a function elevator accepting 3 arguments (in order):
 
-# In the case where both elevators are equally distant from the called floor, choose the elevator to the right.
+left - The current floor of the left elevator
 
-# You can assume that the inputs will always be valid integers between 0-2.
+right - The current floor of the right elevator
 
-# Examples:
+call - The floor that called an elevator
 
-# elevator(0, 1, 0) # => "left"
-# elevator(0, 1, 1) # => "right"
-# elevator(0, 1, 2) # => "right"
-# elevator(0, 0, 0) # => "right"
-# elevator(0, 2, 1) # => "right"
+It should return the name of the elevator closest to the called floor ("left"/"right").
 
+In the case where both elevators are equally distant from the called floor, choose the elevator to the right.
+
+You can assume that the inputs will always be valid integers between 0-2.
+
+Examples:
+```
+elevator(0, 1, 0) # => "left"
+elevator(0, 1, 1) # => "right"
+elevator(0, 1, 2) # => "right"
+elevator(0, 0, 0) # => "right"
+elevator(0, 2, 1) # => "right"
+```
+
+---
+
+### Solution:
+
+```python
 def elevator(left, right, call):
     if abs(call - left) < abs(call - right):
         return "left"
     else:
         return "right"
+```
