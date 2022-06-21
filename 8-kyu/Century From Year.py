cc4ab8a13f57cc1b9c2a402acf2788102caeeeb7
D3schizatorul <106253049+D3schizatorul@users.noreply.github.com>
2022-06-21 15:40:50 +0300

    Update Century From Year.py

--- 8-kyu/Century From Year.py
+++ 8-kyu/Century From Year.py
@@ -1,20 +1,31 @@
-# Introduction
-# The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+# Century From Year
 
-# Task
-# Given a year, return the century it is in.
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
 
-# Examples
-# 1705 --> 18
-# 1900 --> 19
-# 1601 --> 17
-# 2000 --> 20
+Given a year, return the century it is in.
 
-# Solution 1
+Examples
+
+```
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+```
+
+---
+
+### Solution 1:
+
+```python
 from math import ceil
 def century(year):
     return ceil(year / 100)
-  
-# Solution 2
+```
+
+### Solution 2:
+
+```python
 def century(year):
     return (year + 99) // 100
+```
